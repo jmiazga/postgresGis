d5e9e7fae807f5a42a2ddf5bade65fc4631b6eb1
Jason Miazga <jmiazga@gmail.com>
2018-08-07 15:37:04 -0400

    use ST_MakePoint instead of ST_GeogFromText

--- README.md
+++ README.md
@@ -18,12 +18,12 @@ connect using your favorite sql editor then run the following scripts in order:
 
 ### find stores within 1000 meters of -84.5086 39.1003 
 
-SELECT name FROM stores WHERE ST_DWithin(location, ST_GeogFromText('SRID=4326;POINT(-84.5086 39.1003)'), 1000);
+SELECT name FROM stores WHERE ST_DWithin(location, ST_MakePoint(-84.5086, 39.1003), 1000);
 
 ### find stores within 1000000 meters of -84.5086 39.1003, show distance from origin, order by distance from origin
 
 SELECT id, name, ST_Distance(location, ref_geog) AS distance  
 FROM stores  
-CROSS JOIN (SELECT ST_GeogFromText('SRID=4326;POINT(-84.5086 39.1003)') AS ref_geog) AS r  
+CROSS JOIN (SELECT ST_MakePoint(-84.5086, 39.1003) AS ref_geog) AS r  
 WHERE ST_DWithin(location, ref_geog, 1000000)  
 ORDER BY ST_Distance(location, ref_geog); 
--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO stores (name, latitude, longitude, location) VALUES ('Cincinnati', 39.103119, -84.512016, ST_GeogFromText('SRID=4326;POINT(-84.512016 39.103119)') );
-INSERT INTO stores (name, latitude, longitude, location) VALUES ('Cleveland', 41.505493, 81.681290, ST_GeogFromText('SRID=4326;POINT(-81.681290 41.505493)') );
-INSERT INTO stores (name, latitude, longitude, location) VALUES ('Louisville', 38.328732, -85.764771, ST_GeogFromText('SRID=4326;POINT(-85.764771 38.328732)') );
+INSERT INTO stores (name, latitude, longitude, location) VALUES ('Cincinnati', 39.103119, -84.512016, ST_MakePoint(-84.512016, 39.103119) );
+INSERT INTO stores (name, latitude, longitude, location) VALUES ('Cleveland', 41.505493, 81.681290, ST_MakePoint(-81.681290, 41.505493) );
+INSERT INTO stores (name, latitude, longitude, location) VALUES ('Louisville', 38.328732, -85.764771, ST_MakePoint(-85.764771, 38.328732) );
